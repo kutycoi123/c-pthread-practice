6b5d57a1b77e3f93c95fff9f65714f11a7eca72e
kutycoi123 <kutycoi123@gmail.com>
2019-11-13 21:04:54 -0800

    Added codes for file_read

--- assign4/assign4_part1/fileio.c
+++ assign4/assign4_part1/fileio.c
@@ -16,7 +16,28 @@
 
 int file_read(char *path, int offset, void *buffer, size_t bufbytes)
 {
-    return IOERR_NOT_YET_IMPLEMENTED;
+    if(bufbytes <= 0 || !path || !buffer || offset < 0)
+        return IOERR_INVALID_ARGS;
+   /* 
+    FILE *fp;
+    fp = fopen(path, "rb");
+    if(!fp)
+        return IOERR_INVALID_PATH;
+    fseek(fp, offset, SEEK_SET);
+    int readNum = fread(buffer, (int)bufbytes, 2, fp);
+	if(!readNum){
+		
+	}
+    //printf("%d\n", readNum);
+    // int readBytes = readNum * sizeof(buffer)/sizeof(buffer[0]);
+	*/
+	int fd;
+	fd = open(path, O_RDONLY);
+	if(fd == -1)
+		return IOERR_INVALID_PATH;
+	lseek(fd, offset, SEEK_SET);
+	int readBytes = read(fd, buffer, bufbytes);
+    return readBytes;
 }
 
 int file_info(char *path, void *buffer, size_t bufbytes)
